553e533925130397e7c3ff90ca4282390eb9f55d
Ryan Franco <rfranco92@gmail.com>
2017-10-13 01:34:15 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
-
 if (process.env.JAWSDB_URL){
   connection =mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-    host: 'localhost'
-    user: 'root'
-    password: 'a'
+    port: 3306,
+    host: 'localhost,'
+    user: 'root',
+    password: 'a',
     database: "burgers_db"
   })
 }
