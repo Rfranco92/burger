18f3f8fccb2a8c3fd20bc13c3e1b6a991556336c
Ryan Franco <rfranco92@gmail.com>
2017-10-13 01:42:02 -0400

    Fixed Port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log("connected to " + port);
